a0772809726540be55a89bf1adda43764d70d384
Amos <amosakinwale4@gmail.com>
2022-09-12 15:28:27 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Signup from './pages/Signup';
+import Login from './pages/Login'
+import {Routes,Route,Navigate} from "react-router-dom"
 
 const App = () => {
   return (
     <>
     <Routes>
       <Route path="/signup"  element={<Signup/>}/>
-
+      <Route path='/login' element={<Login/>}/>
     </Routes>
   
     </>
