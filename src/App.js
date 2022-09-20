5ed9fc3f82babff231d061ba6d1f8ce90ee3b7a8
Amos <amosakinwale4@gmail.com>
2022-09-20 15:59:06 +0100

    displaying questions and checking if the answer is correct or not

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import Signup from './pages/Signup';
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard';
-import Setquestion from './pages/Setquestion';
-import Takequiz from './pages/Takequiz';
-import {Routes,Route,Navigate} from "react-router-dom"
+import React from "react";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Setquestion from "./pages/Setquestion";
+import Takequiz from "./pages/Takequiz";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-    <Routes>
-      <Route path="/signup"  element={<Signup/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/setquestion' element={<Setquestion/>}/>
-      <Route path='/takequiz' element={<Takequiz/>}/>
-    </Routes>
-  
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/setquestion" element={<Setquestion />} />
+        <Route path="/takequiz" element={<Takequiz />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/pages/Takequiz.js
+++ src/pages/Takequiz.js
@@ -11,49 +11,125 @@ const Takequiz = () => {
   
     
   }, [])
-  const [questionsarray, setquestionsarray] = useState([])
+  // useEffect(()=>{
+  //  console.log(selectedOption)
+  // })
+
+
+  const [questionsObj, setquestionsObj] = useState([])
+  const [score, setscore] = useState(0)
+  const [questions, setquestions] = useState([])
+  const [questionNumber, setquestionNumber] = useState(1)
+
+  const [uid, setuid] = useState("")
+  const [selectedOption, setselectedOption] = useState("")
+
   const getquestions = ()=>{
     let endpointQ =  "http://localhost:5007/user/getquestions"
     let tofind = 2
     let token = localStorage.token
     let details = {tofind, token}
     axios.post(endpointQ, details).then((result)=>{
-      console.log(result)
-      console.log(result.data.result)
       let questionarray = result.data.result
       console.log(questionarray)
-      let filteredquestionarray = questionarray.filter((quest)=>tofind==2)
-      console.log(filteredquestionarray)
-      setquestionsarray(filteredquestionarray)
-      console.log(questionsarray)
-      
+        setquestions(questionarray)
 
+     
+      let a = Math.floor(Math.random()*5)
+      if(a >=questionarray.length){
+        a = 5
+      }
+      
+      setquestionsObj((prev)=>questionarray[a])
+    
+      // setuid(questionsObj.uid)
+      // console.log(questionsObj.uid)
+      setuid(questionarray[a].uid)
     })
-
-    const checkAnswer = ()=>{
-      
-    }
-
-
   } 
+
+  const showUID =()=>{
+   
+    if(selectedOption == questionsObj.correctAnswer){
+      let scores = score +1
+      setscore(scores)
+      
+     
+     
+    }else{
+      // alert("incorrect")
+      console.log(uid)
+    }
+    if(questionNumber>15){
+      alert("maximum no of questions submit")
+    } else{
+      let a = Math.floor(Math.random()*5);
+      setquestionsObj(questions[a])
+      let number = questionNumber + 1
+      setquestionNumber(number)
+     
+
+    }
+   
+  
+  
+  }
+  const submitTest = ()=>{
+
+  }
+
+  // const nextQuestion=()=>{
+    // let a = Math.floor(Math.random()*5);
+    // setquestionsObj(getquestionsarray[a])
+  // }
+  // const checkAnswer = ()=>{
+    // console.log(questionsObj.correctAnswer)
+
+      
+    // let endpointA = "http://localhost:5007/user/checkanswer"
+    // let questiondetails = {uid}
+    // console.log(uid)
+    // axios.post(endpointA, questiondetails).then((result)=>{
+    //   console.log(result)
+    // })
+    
+  // }
   return (
     <>
     <div className='bgquiz'>
+      
         <div className='container bg-light innerquiz'>
-          {
+        <div>{score}/{questionNumber}</div>
+          <div>
+           <p>{questionNumber}: <span>{questionsObj.question}</span></p>
+            a: <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info' value={questionsObj.optionA}/> <span>{questionsObj.optionA}</span><br />
+            b : <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info' value={questionsObj.optionB}/><span>{questionsObj.optionB}</span><br />
+            c : <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info' value={questionsObj.optionC}/><span>{questionsObj.optionC}</span><br />
+            d : <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info' value={questionsObj.optionD}/><span>{questionsObj.optionD}</span><br />
+          
+          </div>
+          <p>{questionsObj.uid}</p>
+          <button className='btn btn-info shadow' onClick={showUID}><i class="fa-solid fa-forward" ></i></button>
+         
+          
+
+          
+          
+          {/* {
             questionsarray.map((quest, index)=>(
+             
               <>
               <h2></h2>
               <b className='text-dark text-center'>{index + 1}.{quest.question}</b><br />
-             a: <input type="button"  className='btn btn-info' value={quest.optionA}/><br />
-             b: <input type="button"  className='btn btn-info my-2' value={quest.optionB}/><br />
-             c : <input type="button" className='btn btn-info my-2' value={quest.optionC}/><br />
-             d : <input type="button" className='btn btn-info my-2' value={quest.optionD}/><br />
-
-             
+             a: <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info' value={quest.optionA}/> <span>{quest.optionA}</span><br />
+             b: <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option'  className='btn btn-info my-2' value={quest.optionB}/> <span>{quest.optionB}</span><br />
+             c : <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option' className='btn btn-info my-2' value={quest.optionC}/><span>{quest.optionC}</span><br />
+             d : <input type="radio" onClick={(e)=>setselectedOption(e.target.value)} name='option' className='btn btn-info my-2' value={quest.optionD}/><span>{quest.optionD}</span><br /> 
+             <button onClick={()=>checkAnswer(quest.uid)}>submit answer</button> 
+         
               </>
             ))
-          }
+          } */}
          
             
 
